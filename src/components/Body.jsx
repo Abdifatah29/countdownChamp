eb13a98beb2d843638ba8f0b716c4de6051c055e
Abdifatah29 <abdi.hassannoor01@gmail.com>
2020-05-07 12:20:27 +0200

    change deadline dynamically

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,8 +4,12 @@ class Body extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            deadline: 'May 25, 2020'
+            deadline: 'May 25, 2020',
+            newDeadline: ''
         }
+    }
+    changeDeadline = () => {
+        this.setState({deadline: this.state.newDeadline})
     }
     render() {
         return (
@@ -18,8 +22,10 @@ class Body extends Component {
                 <div>20 seconds</div>
              </div>
              <div>
-                 <input placeholder="new date"/>
-                 <button>Submit</button>
+                 <input placeholder="new date"
+                     onChange={event => this.setState({newDeadline: event.target.value})}
+                 />
+                 <button onClick={this.changeDeadline}>Submit</button>
              </div>
             </div>
         )
